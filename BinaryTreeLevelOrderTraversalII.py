85b962aa20c2adefb0001183345f71d83f5043d4
Mrtasin <mohammadtasinspj@gmail.com>
2026-04-18 12:21:24 +0530

    add implementation for bottom-up level order traversal of a binary tree

--- BinaryTreeLevelOrderTraversalII.py
+++ BinaryTreeLevelOrderTraversalII.py
@@ -4,6 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution:
     def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ans = []
+        dq = deque([root, None])
+        tempAns = []
+        while dq:
+            temp = dq.popleft()
+            if temp is None:
+                if tempAns:
+                    ans.append(tempAns[:])
+                    tempAns.clear()
+                    if dq:
+                        dq.append(None)
+            else:
+                tempAns.append(temp.val)
+                if temp.left:
+                    dq.append(temp.left)
+                if temp.right:
+                    dq.append(temp.right)
+
+        return ans[::-1]
